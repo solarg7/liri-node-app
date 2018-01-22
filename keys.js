58ae5af8bf80a0440dd0c8a026d780d660d0ec35
Del Solar <marcelo.delsolar@siemens.com>
2018-01-22 08:58:55 -0500

    nodos

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.FAwovnxHJUOLVLEsrk1i2LJ5s,
+  consumer_secret: process.env.Pva5kJhoJjyHPPD1D5McquCwmqdqMF9t2mtuywqlUFUSRZPQl7,
+  access_token_key: process.env.FxgoIgR4I8DYTB6TRn2hhanbHMAWEVtBK30bs5A,
+  access_token_secret: process.env.	mQuF7zmxYwnHg9JYquExw0qOq0UZJCMRolzN6gNw3SvCY
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
